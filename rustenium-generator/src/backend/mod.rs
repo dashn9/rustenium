65be0b419dfa6ad922e7b113bfbf682df8e70ec8
Daniel Shogbon <ishogbon@gmail.com>
2026-02-28 22:04:55 +0100

    introduce mattse/chromiumoxide

---
+++ rustenium-generator/src/backend/mod.rs
@@ -0,0 +1,4 @@
+mod builder;
+mod event;
+pub mod generator;
+mod types;
--- rustenium-generator/src/main.rs
+++ rustenium-generator/src/main.rs
@@ -1,11 +1,10 @@
 
-use crate::frontend::{bidi::parser::parse_cddl_str, cdp::parser::parse_pdl_source, load_file};
+use crate::frontend::{bidi::parser::parse_cddl_str, load_file};
 
 mod frontend;
 mod backend;
 
 pub fn main() {
     let cddl_str = load_file("./raw/cddl/all.cddl");
-    // parse_cddl_str(&cddl_str);
     parse_pdl_source(String::from("./raw/pdl/browser_protocol.pdl"));
 }
